b172ad7ca850fd8fa33849ce40c6eb381cf2984a
empty-michael <michaelttpearce@gmail.com>
2024-07-06 10:59:05 -0700

    add thresh activation

--- factorizer.py
+++ factorizer.py
@@ -24,7 +24,8 @@ class FactorizerConfig():
     theta = 0.3
     activation = 'sigmoid'
     factors = 10000
-    factor_param = 0.5
+    decoder_param = 1
+    factor_param = 1
 
 
 class Factorizer(torch.nn.Module):
@@ -35,36 +36,47 @@ class Factorizer(torch.nn.Module):
         self.decoders = self.decoders / self.decoders.norm(dim=1, keepdim=True) #[n_feat, d_model]
 
         n_feat, d_model = decoders.shape
-        factors = torch.randn(cfg.factors, d_model).to(cfg.device)
+        # factors = torch.randn(cfg.factors, d_model).to(cfg.device)
+        # factors = self.decoders[:cfg.factors]
+        factors = self.decoders[torch.randint(self.decoders.shape[0], size=(self.cfg.factors,))]
         self.factors = torch.nn.Parameter(factors/factors.norm(dim=1,keepdim=True))
 
     def get_factors(self):
         return self.factors / self.factors.norm(dim=1, keepdim=True)
 
-    def sim_weight(self, x):
-        if cfg.activation=='tanh':
+    def sim_activation(self, x):
+        if self.cfg.activation=='tanh':
             return nn.functional.tanh((x/self.cfg.theta)**2)
-        elif cfg.activation=='sigmoid':
+        elif self.cfg.activation=='sigmoid':
             return nn.functional.sigmoid((x-self.cfg.theta)/0.03) + nn.functional.sigmoid(-(x+self.cfg.theta)/0.03)
+        elif self.cfg.activation=='thresh':
+            return (x.abs() > self.cfg.theta).float()
 
     def step(self, batch):
         batch = batch.to(self.cfg.device)
         factors = self.get_factors()
-        d_hat = (batch @ factors.T) @ factors
+        acts = batch @ factors.T
+        acts = self.sim_activation(acts) * acts
+        decoder_hat = acts @ factors
         
-        decoder_sims_hat = d_hat @ d_hat.T
+        mse_loss = (batch - decoder_hat)**2
+        mse_loss = (mse_loss.sum(dim=1) /(batch**2).sum(dim=1)).mean()
+
+        decoder_sims_hat = decoder_hat @ decoder_hat.T
         decoder_sims = batch @ batch.T
-        weight = torch.nn.functional.tanh((decoder_sims/self.cfg.theta)**2)
-        decoder_sim_loss = torch.mean(weight * (decoder_sims - decoder_sims_hat)**2)
+        weight = self.sim_activation(decoder_sims)
+        decoder_sim_loss = weight * (decoder_sims - decoder_sims_hat)**2
+        decoder_sim_loss = (decoder_sim_loss.sum(dim=1)/(weight*decoder_sims**2).sum(dim=1)).mean()
 
         factor_sims = factors @ factors.T
         identity = torch.eye(*factor_sims.shape).to(self.cfg.device)
-        weight = torch.nn.functional.tanh((factor_sims/self.cfg.theta)**2)
-        factor_sim_loss = torch.mean(weight * (factor_sims - identity)**2)
+        weight = self.sim_activation(factor_sims)
+        factor_sim_loss = weight * (factor_sims - identity)**2
+        factor_sim_loss = (factor_sim_loss.sum(dim=1)/(weight*identity**2).sum(dim=1)).mean()
 
-        loss = decoder_sim_loss + self.cfg.factor_param * factor_sim_loss
+        loss = mse_loss + self.cfg.decoder_param * decoder_sim_loss + self.cfg.factor_param * factor_sim_loss
 
-        return loss, decoder_sim_loss, factor_sim_loss
+        return loss, mse_loss, decoder_sim_loss, factor_sim_loss
 
     def fit(self):
         if self.cfg.log: wandb.init(project=self.cfg.wandb_project, config=self.cfg)
@@ -78,15 +90,15 @@ class Factorizer(torch.nn.Module):
         for _ in pbar:
             epoch = []
             for batch in dataloader:
-                loss, decoder_sim_loss, factor_sim_loss = self.train().step(batch)
-                epoch += [(loss.item(), decoder_sim_loss.item(), factor_sim_loss.item())]
+                loss, mse_loss, decoder_sim_loss, factor_sim_loss = self.train().step(batch)
+                epoch += [(loss.item(), mse_loss.item(), decoder_sim_loss.item(), factor_sim_loss.item())]
                     
                 optimizer.zero_grad()
                 loss.backward()
                 optimizer.step()
                 scheduler.step()
             
-            labels = ['loss', 'dec sim loss', 'factor loss']
+            labels = ['loss', 'mse_loss', 'dec sim loss', 'factor loss']
             metrics = {label: sum([step[i] for step in epoch]) / len(epoch) for i, label in enumerate(labels)}
 
             if self.cfg.log: wandb.log(metrics)
